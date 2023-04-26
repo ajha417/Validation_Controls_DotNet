f6266ad475d6ed5e45037dbd4ba79e168b2bc11c
ajha417 <ajha417@rku.ac.in>
2023-04-26 09:11:10 +0530

    adds clientside state management i.e Cookie

---
+++ Validation_Control/Views/Cookies.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Cookies.aspx.cs" Inherits="Validation_Control.Views.Cookies" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="margin:10px">
+            <asp:Button ID="createIt" runat="server" Text="Create Cookie" style="margin-right:10px" OnClick="createIt_Click"/>
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <asp:Label ID="InfoMsg" runat="server" />
+        </div>
+        <div>
+            <asp:Button ID="retrieveIt" runat="server" Text="Retrieve Cookie" OnClick="retrieveIt_Click" />
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Validation_Control/Views/Cookies.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Validation_Control.Views
+{
+    public partial class Cookies : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void createIt_Click(object sender, EventArgs e)
+        {
+            Response.Cookies["name"].Value = TextBox1.Text;
+            Response.Cookies["name"].Expires= DateTime.Now.AddMinutes(1);
+            InfoMsg.Text = "Cookie created";
+            TextBox1.Text = "";
+        }
+
+        protected void retrieveIt_Click(object sender, EventArgs e)
+        {
+            if (Request.Cookies["name"] == null)
+            {
+                TextBox2.Text = "No cookies found";
+            }
+            else
+            {
+                TextBox2.Text = Request.Cookies["name"].Value;
+            }
+        }
+    }
+}
